2a2813d866467529c3e41045e8c98813493a2b60
MariaDuda <mariabevenuto062@gmail.com>
2022-07-29 20:32:53 -0300

    feat customerAdress

--- test.sql
+++ test.sql
@@ -20,3 +20,14 @@ CREATE TABLE "customerPhones"{
  number  NUMERIC NOT NULL
  type VARCHAR (15) NOT NULL
 };
+
+--4
+CREATE TABLE "customerAddresses" (
+	id SERIAL NOT NULL PRIMARY KEY,
+	"customerId" INTEGER NOT NULL  REFERENCES customers(id),
+	street TEXT NOT NULL,
+	number TEXT NOT NULL,
+	complement TEXT,
+	"postalCode" TEXT NOT NULL,
+	"cityId" INTEGER NOT NULL REFERENCES cities(id)
+);
